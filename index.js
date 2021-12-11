78544e9c8c42b03c6e17a7a79667513ac3591bb0
CHRIS_LEE <chrismlee87@gmail.com>
2021-12-11 01:34:07 -0800

    Broken refactor, may revert to bundle module

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-import {
-  select,
-  json,
-  tsv,
-  geoPath,
-  geoNaturalEarth1,
-  event
-} from 'd3';
-import { feature } from 'topojson';
+// import * as d3 from 'https://unpkg.com/d3?module'
+// import * as selection from 'https://unpkg.com/d3-selection?module'
+// import {
+//   select,
+//   json,
+//   tsv,
+//   geoPath,
+//   geoNaturalEarth1,
+//   event,
+// } from 'd3';
+// import { feature } from 'topojson';
 
 const svg = select('svg');
 
@@ -16,14 +18,14 @@ const pathGenerator = geoPath().projection(projection);
 const g = svg.append('g');
 
 g.append('path')
-    .attr('class', 'sphere')
-    .attr('d', pathGenerator({type: 'Sphere'}));
+  .attr('class', 'sphere')
+  .attr('d', pathGenerator({ type: 'Sphere' }));
 
 Promise.all([
-  tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),
-  json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
+  d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),
+  d3.json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
 ]).then(([tsvData, topoJSONdata]) => {
-  
+
   const countryName = tsvData.reduce((accumulator, d) => {
     accumulator[d.iso_n3] = d.name;
     return accumulator;
@@ -32,9 +34,8 @@ Promise.all([
   const countries = feature(topoJSONdata, topoJSONdata.objects.countries);
   g.selectAll('path').data(countries.features)
     .enter().append('path')
-      .attr('class', 'country')
-      .attr('d', pathGenerator)
+    .attr('class', 'country')
+    .attr('d', pathGenerator)
     .append('title')
-      .text(d => countryName[d.id]);
-  
+    .text(d => countryName[d.id]);
 });
